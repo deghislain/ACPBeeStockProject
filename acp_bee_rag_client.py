6e2d835bb54ac04f649bf8444389506c65f64c07
deghislain@gmail.com <deghislain@gmail.com>
2025-03-18 19:48:59 -0300

    code clean up

--- acp_bee_rag_client.py
+++ acp_bee_rag_client.py
@@ -2,7 +2,6 @@ from acp.server.highlevel import Server, Context
 from beeai_sdk.providers.agent import run_agent_provider
 from beeai_sdk.schemas.metadata import UiDefinition, UiType
 from beeai_sdk.schemas.text import TextInput, TextOutput
-from acp_bee_local_model import LocalModel as model
 import logging
 
 from agents import Agent, Runner
@@ -10,16 +9,7 @@ import asyncio
 from acp_bee_rag_tool import RAGTool
 
 
-def init_logger() -> logging.Logger:
-    logging.basicConfig(
-        level=logging.INFO,  # logging.DEBUG,
-        format='\x1b[90m[%(levelname)s]\x1b[0m %(message)s'
-    )
-    return logging.getLogger()
-
-
 async def run():
-    init_logger()
     server = Server("rag-agent")
 
     @server.agent(
